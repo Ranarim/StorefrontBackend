998147b0ad08e404b99c5bf84d8e3a291432b3ed
Johannes <74767916+Ranarim@users.noreply.github.com>
2022-07-21 23:20:21 +0200

    Save changes

--- src/database.ts
+++ src/database.ts
@@ -1,24 +1,12 @@
-import dotenv from "dotenv"
 import {Pool} from "pg"
 
-dotenv.config()
 
-const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_DB_TEST,
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-    ENV,
-  } = process.env
+const pool = new Pool ({
+  user: "johannesmaier",
+  host: "localhost",
+  database: "storefront",
+  password: "geheim",
+  port: 5432,
+})
 
-  const pool = new Pool({
-    host: POSTGRES_HOST,
-    database: ENV === 'dev' ? POSTGRES_DB : POSTGRES_DB_TEST,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    port: 5432,
-  })
-
-export default pool 
-
+export default pool
